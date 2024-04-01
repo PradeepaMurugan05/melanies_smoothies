01552c575d2dc064d657967ef3f079826668b1d6
PradeepaMurugan05 <157470810+PradeepaMurugan05@users.noreply.github.com>
2024-04-01 14:27:48 +0530

    Update streamlit_app.py
    IMPORTING REQUEST LIBRARY

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ time_to_insert=st.button('submit order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(f"Your Smoothie is ordered! {name_on_order}",icon="✅")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 
