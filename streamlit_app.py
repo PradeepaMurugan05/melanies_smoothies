33b69bcb366331e55be638b5bb9449c57b496feb
PradeepaMurugan05 <157470810+PradeepaMurugan05@users.noreply.github.com>
2024-04-01 14:35:31 +0530

    Update streamlit_app.py
    changed into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if time_to_insert:
     st.success(f"Your Smoothie is ordered! {name_on_order}",icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
