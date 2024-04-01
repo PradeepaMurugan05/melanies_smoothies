f25bb5c2599471666f106c5a6c8bd59d11d08867
PradeepaMurugan05 <157470810+PradeepaMurugan05@users.noreply.github.com>
2024-04-01 14:31:46 +0530

    Update streamlit_app.py
    changed into json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if time_to_insert:
     st.success(f"Your Smoothie is ordered! {name_on_order}",icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
 
